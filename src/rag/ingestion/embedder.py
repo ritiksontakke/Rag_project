3dc746120da9c3c51dbb01a8f37545746af6a21f
ritiksontakke <ritiksontakke1008@gmail.com>
2026-08-21 23:35:07 +0530

    edit prompt

--- src/rag/ingestion/embedder.py
+++ src/rag/ingestion/embedder.py
@@ -1,8 +1,9 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 
-
 embeddings = HuggingFaceEmbeddings(
     model_name="BAAI/bge-small-en-v1.5",
+    model_kwargs={"device": "cpu"},
+    encode_kwargs={"normalize_embeddings": True},
 )
 
 
