9ff176ffd1971a21f8779edfbdaf34a7766e2efc
ritiksontakke <ritiksontakke1008@gmail.com>
2026-08-22 02:02:37 +0530

    Fix CORS for Netlify frontend

--- src/main.py
+++ src/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 from fastapi.middleware.cors import CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500","https://multi-model-rag-frontend.netlify.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
