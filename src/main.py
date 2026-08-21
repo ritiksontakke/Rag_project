2517dc0a08807a6a04402535305727f550a5b831
ritiksontakke <ritiksontakke1008@gmail.com>
2026-08-21 23:54:13 +0530

    edit prompt

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from src.api.v1.auth import router as users_router
 from src.api.v1.documents import router
 from fastapi import FastAPI
-from src.core.config import create_qdrant_collection
+# from src.core.config import create_qdrant_collection
 from src.models.user import User
 from src.api.v1.knowledge import router as knowledge_router
 from src.db.init_db import create_tables
@@ -23,10 +23,10 @@ app.add_middleware(
 )
 
 
-@app.on_event("startup")
-def startup_event():
+# @app.on_event("startup")
+# def startup_event():
 
-    create_qdrant_collection()
+#     create_qdrant_collection()
 
 
 create_tables()
