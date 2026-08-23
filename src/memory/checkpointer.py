e02cb2236472cccf3d19edede61e2f68fee660b7
ritiksontakke <ritiksontakke1008@gmail.com>
2026-08-23 23:19:23 +0530

    improve prompt

--- src/memory/checkpointer.py
+++ src/memory/checkpointer.py
@@ -1,10 +1,23 @@
 from langgraph.checkpoint.postgres import PostgresSaver
 
-from src.db.database import DATABASE_URL
+from src.db.database import (
+    USER,
+    PASSWORD,
+    HOST,
+    PORT,
+    DBNAME,
+)
+
+
+POSTGRES_CHECKPOINT_URL = (
+    f"postgresql://"
+    f"{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}"
+    f"?sslmode=require"
+)
 
 
 checkpointer_context = PostgresSaver.from_conn_string(
-    DATABASE_URL
+    POSTGRES_CHECKPOINT_URL
 )
 
 checkpointer = checkpointer_context.__enter__()
